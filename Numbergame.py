603d015eaa13e28e452fc61dd370d1efdb0adbd2
MIMIC9899 <92639781+MIMIC9899@users.noreply.github.com>
2024-03-16 16:32:37 +0700

    Update Numbergame.py

--- Numbergame.py
+++ Numbergame.py
@@ -1,6 +1,5 @@
-#  Number game 
-
 import paramiko 
+import time
 
 host = 'challenges.ringzer0team.com'
 user = 'number'
@@ -15,30 +14,19 @@ s = ssh.recv(3000)
 print(s)
 s = ssh.recv(3000)
 print(s)
-numb = 5000
-difference = [2200,600,170,50,15,4,1]
-i = 0
-fSmall = 0
-fBig = 0
-while(True):
-    if b"win" in s:
-        i=0
-        numb = 5000
+low = 0
+high = 10000
+while (True):
+    if b'win' in s:
+        low = 0
+        high = 10000
         print(s)
-    q = ssh.send(f"{numb}\n")
+    numb = (low + high)//2
+    q = ssh.send(str(numb) + "\n")
+    time.sleep(0.2)
     s = ssh.recv(1000)
+    #print(low, high, s)
     if b"small" in s:
-        numb += difference[i]
-        fSmall = 1
-        if fSmall == 0:
-            i+=1
-        fBig = 0
-    elif b"big" in s:
-        numb = difference[i]
-        fSmall = 0
-        if fBig == 0:
-            i+=1
-        fBig = 1
-    if b"10 on 10" in s:
-        break
-print(ssh.recv(1000))
+        low = numb
+    if b"big" in s:
+        high = numb
